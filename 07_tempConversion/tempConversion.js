16255360a9f3072f95d9a0730faac39af3f2460c
saucedo111 <99573171+saucedo111@users.noreply.github.com>
2023-09-25 09:23:29 -0400

    finished last 2, move onto nextstep for now

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+    let ret = 5 / 9 * (num - 32);
+    ret = ret.toFixed(1);
+    return parseFloat(ret);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+    let ret = 9 / 5 * num + 32;
+    ret = ret.toFixed(1);
+    return parseFloat(ret);
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius, convertToFahrenheit
 };
